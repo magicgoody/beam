c87ab0fff3b8b33789ae84070267fd839ecd1a1a
Timur Sultanov <timur.sultanov@akvelon.com>
2023-03-30 17:07:27 +0400

    Remove duplicated code from external_functions_component.go

--- playground/backend/internal/components/external_functions_component.go
+++ playground/backend/internal/components/external_functions_component.go
@@ -32,26 +32,32 @@ func NewExternalFunctionsComponent() ExternalFunctions {
 	return &externalFunctionsComponent{}
 }
 
+func makePostRequest(ctx context.Context, requestUrl string) error {
+	request, err := http.NewRequestWithContext(ctx, "POST", requestUrl, nil)
+	if err != nil {
+		logger.Errorf("makePostRequest(): Couldn't create the request, err: %s\n", err.Error())
+		return err
+	}
+
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		logger.Errorf("makePostRequest(): Couldn't make POST request to the %s, err: %s\n", requestUrl, err.Error())
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
 func (c *externalFunctionsComponent) CleanupSnippets(ctx context.Context) error {
 	requestUrl := fmt.Sprintf("%s", cleanupSnippetsFunctionsUrl)
 
-	request, err := http.NewRequestWithContext(ctx, "POST", requestUrl, nil)
-	if err != nil {
-		logger.Errorf("CleanupSnippets(): Couldn't create the request, err: %s\n", err.Error())
+	if err := makePostRequest(ctx, requestUrl); err != nil {
+		logger.Errorf("CleanupSnippets(): Couldn't cleanup snippets, err: %s\n", err.Error())
 		return err
-	}
-
-	// Make POST request to the requestUrl
-	resp, err := http.DefaultClient.Do(request)
-	if err != nil {
-		logger.Errorf("CleanupSnippets(): Couldn't make POST request to the %s, err: %s\n", requestUrl, err.Error())
-		return err
-	}
-
-	// Check the response status code
-	if resp.StatusCode != http.StatusOK {
-		logger.Errorf("CleanupSnippets(): Couldn't cleanup snippets, status code: %d\n", resp.StatusCode)
-		return fmt.Errorf("Couldn't cleanup snippets, status code: %d", resp.StatusCode)
 	}
 
 	return nil
@@ -60,23 +66,9 @@ func (c *externalFunctionsComponent) CleanupSnippets(ctx context.Context) error 
 func (c *externalFunctionsComponent) DeleteObsoleteSnippets(ctx context.Context, snipId string, persistenceKey string) error {
 	requestUrl := fmt.Sprintf("%s?snipId=%s&persistenceKey=%s", deleteObsoleteSnippetsFunctionsUrl, snipId, persistenceKey)
 
-	// Make POST request to the requestUrl
-	request, err := http.NewRequestWithContext(ctx, "POST", requestUrl, nil)
-	if err != nil {
-		logger.Errorf("DeleteObsoleteSnippets(): Couldn't create the request, err: %s\n", err.Error())
+	if err := makePostRequest(ctx, requestUrl); err != nil {
+		logger.Errorf("DeleteObsoleteSnippets(): Couldn't delete obsolete snippets, err: %s\n", err.Error())
 		return err
-	}
-
-	resp, err := http.DefaultClient.Do(request)
-	if err != nil {
-		logger.Errorf("DeleteObsoleteSnippets(): Couldn't make POST request to the %s, err: %s\n", requestUrl, err.Error())
-		return err
-	}
-
-	// Check the response status code
-	if resp.StatusCode != http.StatusOK {
-		logger.Errorf("DeleteObsoleteSnippets(): Couldn't delete obsolete snippets, status code: %d\n", resp.StatusCode)
-		return fmt.Errorf("Couldn't delete obsolete snippets, status code: %d", resp.StatusCode)
 	}
 
 	return nil
@@ -85,23 +77,9 @@ func (c *externalFunctionsComponent) DeleteObsoleteSnippets(ctx context.Context,
 func (c *externalFunctionsComponent) IncrementSnippetViews(ctx context.Context, snipId string) error {
 	requestUrl := fmt.Sprintf("%s?snipId=%s", incrementSnippetViewsFunctionsUrl, snipId)
 
-	// Make POST request to the requestUrl
-	request, err := http.NewRequestWithContext(ctx, "POST", requestUrl, nil)
-	if err != nil {
-		logger.Errorf("IncrementSnippetViews(): Couldn't create the request, err: %s\n", err.Error())
+	if err := makePostRequest(ctx, requestUrl); err != nil {
+		logger.Errorf("IncrementSnippetViews(): Couldn't increment snippet views, err: %s\n", err.Error())
 		return err
-	}
-
-	resp, err := http.DefaultClient.Do(request)
-	if err != nil {
-		logger.Errorf("IncrementSnippetViews(): Couldn't make POST request to the %s, err: %s\n", requestUrl, err.Error())
-		return err
-	}
-
-	// Check the response status code
-	if resp.StatusCode != http.StatusOK {
-		logger.Errorf("IncrementSnippetViews(): Couldn't increment snippet views, status code: %d\n", resp.StatusCode)
-		return fmt.Errorf("Couldn't increment snippet views, status code: %d", resp.StatusCode)
 	}
 
 	return nil
