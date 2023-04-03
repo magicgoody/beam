8a9dd87440833844a2ff8366a147737df3776fb7
Sergey Makarkin <yahot@mail.ru>
2023-04-03 18:39:23 +0500

    Change var volume

--- playground/terraform/infrastructure/cloudfunctions/variables.tf
+++ playground/terraform/infrastructure/cloudfunctions/variables.tf
@@ -21,9 +21,9 @@ variable "gkebucket" {
   description = "Bucket name for CloudFunction"
 }
 
-variable "project_id" {
-  description = "The GCP Project ID where Playground Applications will be created"
-}
+# variable "project_id" {
+#  description = "The GCP Project ID where Playground Applications will be created"
+#}
 
 variable "service_account_email_cf" {
   description = "Service account email for CloudFunctions"
