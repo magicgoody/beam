535bbb8fbfc2123cc459d822d28c962d8ca3f6b0
Sergey Makarkin <yahot@mail.ru>
2023-04-04 18:17:46 +0500

    rebuild optimization

--- playground/terraform/infrastructure/cloudfunctions/output.tf
+++ playground/terraform/infrastructure/cloudfunctions/output.tf
@@ -18,13 +18,13 @@
 #
 
 output "playground_function_cleanup_url" {
-  value = google_cloudfunctions_function.playground_functions[count.index].https_trigger_url
+  value = google_cloudfunctions_function.playground_functions[0].https_trigger_url
 }
 
 output "playground_function_delete_url" {
-  value = google_cloudfunctions_function.playground_functions[count.index].https_trigger_url
+  value = google_cloudfunctions_function.playground_functions[1].https_trigger_url
 }
 
 output "playground_function_view_url" {
-  value = google_cloudfunctions_function.playground_functions[count.index].https_trigger_url
+  value = google_cloudfunctions_function.playground_functions[2].https_trigger_url
 }
