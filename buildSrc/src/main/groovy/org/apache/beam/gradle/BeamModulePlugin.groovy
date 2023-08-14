cfa561f092cf1489a2088c373038e8768dcd0b67
magicgoody <magzhan.kulatayev@akvelon.com>
2023-08-14 19:17:11 +0600

    xml enabled

--- buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
+++ buildSrc/src/main/groovy/org/apache/beam/gradle/BeamModulePlugin.groovy
@@ -1339,9 +1339,9 @@ class BeamModulePlugin implements Plugin<Project> {
         }
         project.tasks.withType(com.github.spotbugs.snom.SpotBugsTask) {
           reports {
-            html.enabled = project.jenkins.isCIBuild
-            xml.enabled = !project.jenkins.isCIBuild
-            // xml.enabled = project.jenkins.isCIBuild || !project.jenkins.isCIBuild
+            xml {
+              enabled = true
+            }
           }
         }
       }
